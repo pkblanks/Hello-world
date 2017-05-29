2e1e90d844edd6f6e313bfcd100cd9879b2d1180
pkblanks <pkblanks@yahoo.com>
2017-05-29 11:22:45 +0100

    Added first python practice using sublime text 3

---
+++ conditional-flow.py
@@ -0,0 +1,52 @@
+# #example 1
+# name = 'Bertha'
+# female = 'Bertha'
+
+# if name == female:
+# 	print 'Welcome'
+# else:
+# 	print "No"
+
+# #example 2
+# name = 'Bertha'
+# female = 'Alex'
+
+# if name == female:
+# 	print 'Welcome'
+# else:
+# 	print "No"
+
+#example 3
+# name = raw_input("what is your favorite car?")
+# if name == 'Range Rover':
+
+# 	print 'Tata'
+# else:
+# 	print name 
+
+#example 4
+# name = raw_input("what is your favorite car?")
+# if name == 'Range Rover':
+# 	print 'it was manufactured by Tata'
+# else: 
+# 	print name  
+# 	new_name = raw_input("Manufacturer?")
+# 	print "it was manufactured by" + new_name 
+
+#example 5
+# name = raw_input ("what is your favorite car?")
+# if name == 'Range Rover':
+# 	print 'it was manufactured by Tata'
+# elif name == "toyota":  
+# 	print "Toyota Company Limited"
+# else:
+# 	print "I dont know"
+
+# #sample 1
+
+# #sample 1
+num = 20
+if num == 20:
+	print "print the number is 20"
+else: 
+	print "the number is not 20"

d0d484b68afa5d806f221bdb8cd3210e862510a2
pkblanks <pkblanks@yahoo.com>
2017-05-31 21:56:59 +0100

    Added Recap

---
+++ Recap.py
@@ -0,0 +1,57 @@
+#Numbers
+# #Example of numbers is 
+# floatNumber = 0.1
+# Integer = 20
+# print (floatNumber + Integer) 
+
+# #String
+# print "Example of Python string is Welcome"
+
+# #List
+# Ghana = ["Accra", "Kumasi", "Cape Coast", "Tamale", "Sunyani"]
+# print Ghana
+
+# #Tuple
+# Today = ("Sunday", 100, "Accra", 2.25 )
+# print (Today)
+
+# #Dictionary
+# WA = {"Ghana":"Accra", "Nigeria": "Abuja", "Liberia":"Monrovia", "Sierra Leone": "Free Town"}
+# print (WA)
+
+
+#Numbers
+
+#String
+# print1 = "Example of Python string is Welcome"
+# print print1 [17:24]
+
+# # #List
+# Ghana = ["Accra", "Kumasi", "Cape Coast", "Tamale", "Sunyani"]
+# print Ghana[2]
+
+# # #Tuple
+# Today = ("Sunday", 100, "Accra", 2.25 )
+# print Today[2]
+
+# # #Dictionary
+# WA = {"Ghana":"Accra", "Nigeria": "Abuja", "Liberia":"Monrovia", "Sierra Leone": "Free Town"}
+# print WA 
+
+#Recap 3
+# name = raw_input('What is the name of your Laptop?')
+# price = "Toshiba"
+# if name == price:
+#  	print 'Congrats!! You have won a brand New Toshiba Laptop'
+# elif name == "Macbook":  
+#  	print "Send GH2,500 to Mobile Money Number 0244060400 for your new Macbook"
+# else:
+#  	print "Your Reward is in Heaven!!!"
+
+# name = raw_input("Hello, What is your name?")
+# print "welcome %s" %name, "Hurray!! you have won a car"
+# car = raw_input("what is your favorite car?")
+# if car == "Range Rover":
+# 	print "call 0244060400 for your reward"
+# else: 
+# 	print "your choice is not available, try next time!!!"

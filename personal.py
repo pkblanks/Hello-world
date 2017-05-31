6990f066ec362694eba69e7d92683d0a7997fee1
pkblanks <pkblanks@yahoo.com>
2017-05-31 22:08:30 +0100

    AddednRecap

---
+++ personal.py
@@ -0,0 +1,18 @@
+# name = raw_input("what is your name?")
+# print "Welcome %s!" %name, "Hurray!! you have won a car"
+# car = raw_input ("what is your favorite car?")
+# if car == 'Range Rover':
+# 	print 'it was manufactured by Tata'
+# elif car == "toyota":  
+# 	print "Toyota Company Limited"
+# else:
+# 	print "I dont know"
+
+name = raw_input("Hello, What is your name?")
+print "welcome %s" %name, "Hurray!! you have won a car"
+car = raw_input("what is your favorite car?")
+if car == "Range Rover":
+	print "call 0244060400 for your reward"
+else: 
+	print "your choice is not available, try next time!!!"
+
